120b7c8ccd967ca5c8cab60a986c206bacb072da
Keunhong Lee <dlrmsghd@gmail.com>
2015-08-01 17:35:31 +0900

    White space update.

--- src/bitalloc.rs
+++ src/bitalloc.rs
@@ -11,123 +11,121 @@ pub trait Bitalloc {
 }
 
 impl<T: ilog2::Bitops + Binary> Bitalloc for [T] {
-	fn clear_all(&mut self) {
-		for item in self.iter_mut() {
-			*item = T::zero();
-		}
-	}
-	fn fill_all(&mut self) {
-		for item in self.iter_mut() {
-			*item = ilog2::bit_mask::<T>();
-		}
-	}
-	fn alloc(&mut self, size: usize) -> isize {
-	    let block_size = ilog2::bit_length::<T>() as usize;
-	    let block_count = size / block_size;
-	    let rem_count = size % block_size;
-	    
-	    let mut blocks_allocated = 0usize;
-	    let mut rem_allocated = 0usize;
-	    let mut blocks_needed = block_count;
-	    let mut rem_needed = rem_count;
-	    
-	    enum State {
-	        FindFirst,
-	        FindMid,
-	        FindLast,
-	    };
-	    
-	    let mut found_index = 0usize;
-	    let mut found_offset = 0usize;
-	    let mut found = false;
-	    let mut state = State::FindFirst;
-	    let mut index = 0usize;
-	    let limit = self.len();
-	    loop {
-	        if index == limit {
-	            break;
-	        }
-	        let val = self[index];
-	        
-	        match state {
-	            State::FindFirst => {
-	                //reset
-	                blocks_allocated = 0;
-	                rem_allocated = 0;
-	                blocks_needed = block_count;
-	                rem_needed = rem_count;
-	                found_index = 0;
-	                found_offset = 0;
-	                
-	                if size <= block_size { // allocated region may inside an integer
-	                    let mut possible = val;
-	                    let mut shifted = 0usize;
-	                    loop {
-	                        let current_available = possible.leading_zeros() as usize;
-	                        //println!("debug2 {} {} {} {:064b}", current_available, size, shifted, possible);
-	                        if current_available >= size {
-	                            found = true;
-	                            found_index = index;
-	                            found_offset = shifted;
-	                            break;
-	                        }
-	                        possible = possible << current_available; //discard current;
-	                        shifted += current_available;
-	                        //println!("debug3 {} {} {} {:064b}", current_available, size, shifted, possible);
-	                        let current_skip = possible.leading_ones() as usize;
-	                        shifted += current_skip;
-	                        if (shifted + size) > block_size {
-	                            break;
-	                        }
-	                        possible = possible << current_skip;
-	                        possible = possible | ilog2::bit_mask::<T>() >> (block_size - shifted);
-	                        //println!("debug4 {} {} {} {:064b}", current_available, size, shifted, possible);
-	                    }
-	                    if found {
-	                        break;
-	                    }
-	                }
-	                //size exceeded block size or failed to allocated inside a block
-	                //find zeros from lsb position	                
-	                let current_available = val.trailing_zeros() as usize;
-	                if current_available > 0 {
-	                    //possible start location
+    fn clear_all(&mut self) {
+        for item in self.iter_mut() {
+            *item = T::zero();
+        }
+    }
+    fn fill_all(&mut self) {
+        for item in self.iter_mut() {
+            *item = ilog2::bit_mask::<T>();
+        }
+    }
+    fn alloc(&mut self, size: usize) -> isize {
+        let block_size = ilog2::bit_length::<T>() as usize;
+        let block_count = size / block_size;
+        let rem_count = size % block_size;
+        
+        let mut blocks_allocated = 0usize;
+        let mut rem_allocated = 0usize;
+        let mut blocks_needed = block_count;
+        let mut rem_needed = rem_count;
+        
+        enum State {
+            FindFirst,
+            FindMid,
+            FindLast,
+        };
+        
+        let mut found_index = 0usize;
+        let mut found_offset = 0usize;
+        let mut found = false;
+        let mut state = State::FindFirst;
+        let mut index = 0usize;
+        let limit = self.len();
+        loop {
+            if index == limit {
+                break;
+            }
+            let val = self[index];
+            
+            match state {
+                State::FindFirst => {
+                    //reset
+                    blocks_allocated = 0;
+                    rem_allocated = 0;
+                    blocks_needed = block_count;
+                    rem_needed = rem_count;
+                    found_index = 0;
+                    found_offset = 0;
+                    
+                    if size <= block_size { // allocated region may inside an integer
+                        let mut possible = val;
+                        let mut shifted = 0usize;
+                        loop {
+                            let current_available = possible.leading_zeros() as usize;
+                            //println!("debug2 {} {} {} {:064b}", current_available, size, shifted, possible);
+                            if current_available >= size {
+                                found = true;
+                                found_index = index;
+                                found_offset = shifted;
+                                break;
+                            }
+                            possible = possible << current_available; //discard current;
+                            shifted += current_available;
+                            //println!("debug3 {} {} {} {:064b}", current_available, size, shifted, possible);
+                            let current_skip = possible.leading_ones() as usize;
+                            shifted += current_skip;
+                            if (shifted + size) > block_size {
+                                break;
+                            }
+                            possible = possible << current_skip;
+                            possible = possible | ilog2::bit_mask::<T>() >> (block_size - shifted);
+                            //println!("debug4 {} {} {} {:064b}", current_available, size, shifted, possible);
+                        }
+                        if found {
+                            break;
+                        }
+                    }
+                    //size exceeded block size or failed to allocated inside a block
+                    //find zeros from lsb position                    
+                    let current_available = val.trailing_zeros() as usize;
+                    if current_available > 0 {
+                        //possible start location
                         found_index = index;
                         found_offset = block_size - current_available;
-    	                if current_available > rem_needed {
-    	                    assert!(blocks_needed > 0);
-    	                    rem_needed = rem_needed + block_size - current_available;
-    	                    blocks_needed -= 1;
-    	                }
-    	                rem_allocated += current_available;
-    	                state = State::FindMid;
-	                }
-	            },
-	            State::FindMid => {
-	                if blocks_allocated == blocks_needed {
-	                    state = State::FindLast;
-	                    continue;
-	                } else if val == T::zero() {
-	                    blocks_allocated += 1;
-	                }
-	            },
-	            State::FindLast => {
-	                let current_available = val.leading_zeros() as usize;
-	                rem_allocated += current_available;
-	                if rem_allocated >= rem_needed {
-	                    found = true;
-	                    break;
-	                }
-	                state = State::FindFirst;
-	                continue;
-	            },
-	        }
-	        index += 1;
-	    }
-	    if found {
-	        
-	        let mut allocated = 0usize;
-	        
+                        if current_available > rem_needed {
+                            assert!(blocks_needed > 0);
+                            rem_needed = rem_needed + block_size - current_available;
+                            blocks_needed -= 1;
+                        }
+                        rem_allocated += current_available;
+                        state = State::FindMid;
+                    }
+                },
+                State::FindMid => {
+                    if blocks_allocated == blocks_needed {
+                        state = State::FindLast;
+                        continue;
+                    } else if val == T::zero() {
+                        blocks_allocated += 1;
+                    }
+                },
+                State::FindLast => {
+                    let current_available = val.leading_zeros() as usize;
+                    rem_allocated += current_available;
+                    if rem_allocated >= rem_needed {
+                        found = true;
+                        break;
+                    }
+                    state = State::FindFirst;
+                    continue;
+                },
+            }
+            index += 1;
+        }
+        if found {
+            let mut allocated = 0usize;
             let mut first_mask = ilog2::bit_mask::<T>();
             first_mask = first_mask >> found_offset;
             allocated += block_size - found_offset;
@@ -170,8 +168,8 @@ impl<T: ilog2::Bitops + Binary> Bitalloc for [T] {
             assert!(allocated == size);
             
             ((found_index * block_size) + found_offset) as isize
-	    } else {
-	        -1isize	        
-	    }
-	}
+        } else {
+            -1isize            
+        }
+    }
 }
