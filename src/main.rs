2ab3489276873bf2db8ed64eac676fc05fb11be0
Keunhong Lee <dlrmsghd@gmail.com>
2015-08-01 17:27:02 +0900

    Test update.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ extern crate bitalloc;
 use bitalloc::Bitalloc;
 fn main() {
     let mut array = [0u64; 128];
+    let mut array2 = [0u8; 128*8];
     let slice =&mut  array;
-    for _ in 0..320 {
-        println!("allocated {}", slice.alloc(64*2 + 13));
+    let slice2 =&mut  array2;
+    for _ in 0..32 {
+        println!("allocated {} {}", slice.alloc(64*2 + 13), slice2.alloc(64*2 + 13));
     }
 }
